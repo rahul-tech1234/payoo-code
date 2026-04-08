e8fe117728414d9f7131f5d59e0795e40a8a3734
rahul-tech1234 <rahul.laravel.dev@gmail.com>
2026-04-08 23:54:55 +0600

    compleate cashout page with daynamic

---
+++ script/cashout.js
@@ -0,0 +1,41 @@
+document.getElementById("cahout-btn").addEventListener("click", function () {
+    //1 - get agent number &valid
+    const cashOutNumberInput = document.getElementById("cashout-number");
+    const cashOutNumberValue = cashOutNumberInput.value;
+    if (cashOutNumberValue.length != 11) {
+        alert("Invalid Agent Number");
+        return 
+    }
+    //console.log(cashOutValue);
+    //2 - get the amount
+    const amount = document.getElementById("amount");
+    const amountValue = amount.value;
+    
+    //console.log(amountValue);
+    //3 - get current balance ,validate, convert number
+    const balanceElement = document.getElementById("balance");
+    const oldBalance = balanceElement.innerText;
+    //console.log(oldBalance);
+    //4 - calculate new balance
+    const newBalance = Number(oldBalance) - Number(amountValue);
+
+    if (newBalance < 0) {
+        alert("Invalid Amount");
+        return;
+    }
+    //5 - get the pin varify
+    const pinNumber = document.getElementById("cahout-pin");
+    const pinValue =pinNumber.value;
+    if (pinValue === '1234') {
+        //5 -1 true show an alert > set balance
+        console.log("cashout successfull");
+        console.log("newBalance", newBalance);
+        balanceElement.innerText =newBalance;
+    }else{
+        //5 -2 flase show anerror alert > return
+        alert('Invalid pin');
+        return
+    }
+    
+    
+});
